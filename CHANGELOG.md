a91f07d0ac6c352c9b60ad33aa3ac585016446b2
forfd8960 <forfd8960@gmail.com>
2024-08-19 20:27:32 +0800

    feat: add Add and Commit SubCommand

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,43 @@
 # Change Log
 
+
+## 24-08-19
+
+* Add Add Sub-Command
+* Add Commit Sub-Command
+
+```sh
+cargo run -- help add
+   Compiling git-rs v0.1.0
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 1.32s
+     Running `target/debug/git-rs help add`
+add files into index
+
+Usage: git-rs add [OPTIONS] --path <PATH_SPEC>
+
+Options:
+  -p, --path <PATH_SPEC>  Files to add content from
+  -a, --all               If no <pathspec> is given when -A option is used, all files in the entire working tree are updated
+  -h, --help              Print help
+```
+
+```sh
+cargo run -- help commit
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.09s
+     Running `target/debug/git-rs help commit`
+Create a new commit containing the current contents of the index and the given log message describing the changes
+
+Usage: git-rs commit [OPTIONS] --message <MESSAGE>
+
+Options:
+  -a, --all                Tell the command to automatically stage files that have been modified and deleted, but new files you have not told Git about are not affected
+  -m, --message <MESSAGE>  Record changes to the repository
+  -e, --edit               The message taken from file with -F, command line with -m, and from commit object with -C are usually used as the commit log message unmodified. This option lets you further edit the message taken from these sources
+  -n, --no-edit            Use the selected commit message without launching an editor
+      --amend
+  -h, --help               Print help
+```
+
 ## 24-08-18
 
 * Set up the project.
--- src/command/mod.rs
+++ src/command/mod.rs
@@ -11,6 +11,13 @@ pub struct SimpleGit {
 pub enum GitSubCommand {
     #[command(name = "init", about = "init a git repo")]
     Init(InitOpts),
+    #[command(name = "add", about = "add files into index")]
+    Add(AddOpts),
+    #[command(
+        name = "commit",
+        about = "Create a new commit containing the current contents of the index and the given log message describing the changes"
+    )]
+    Commit(CommitOpts),
 }
 
 #[derive(Debug, Parser)]
@@ -33,4 +40,38 @@ pub struct InitOpts {
     /// Use <branch-name> for the initial branch in the newly created repository. If not specified, fall back to the default name.
     #[arg(short, long = "initial-branch")]
     pub branch: String,
+}
+
+#[derive(Debug, Parser)]
+pub struct AddOpts {
+    /// Files to add content from
+    #[arg(short, long = "path")]
+    pub path_spec: String,
+
+    /// If no <pathspec> is given when -A option is used, all files in the entire working tree are updated
+    #[arg(short, long = "all")]
+    pub all: bool,
+}
+
+#[derive(Debug, Parser)]
+pub struct CommitOpts {
+    /// Tell the command to automatically stage files that have been modified and deleted, but new files you have not told Git about are not affected.
+    #[arg(short, long)]
+    pub all: bool,
+
+    /// Record changes to the repository
+    #[arg(short, long)]
+    pub message: String,
+
+    /// The message taken from file with -F, command line with -m, and from commit object with -C are usually used as the commit log message unmodified. This option lets you further edit the message taken from these sources.
+    #[arg(short, long = "edit")]
+    pub edit: bool,
+
+    /// Use the selected commit message without launching an editor
+    #[arg(short, long = "no-edit")]
+    pub no_edit: bool,
+
+    /// Replace the tip of the current branch by creating a new commit.
+    #[arg(long)]
+    pub amend: bool,
 }
