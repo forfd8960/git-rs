7d33f91449daaf6d19c4d1eaf174985426f8ea0f
forfd8960 <forfd8960@gmail.com>
2026-01-04 21:43:32 +0800

    feat: encode commit

--- src/plumbing/commit/mod.rs
+++ src/plumbing/commit/mod.rs
@@ -60,7 +60,9 @@ type ExtraHeader struct {
 }
 */
 
-use crate::plumbing::object::Signature;
+use chrono::{DateTime, FixedOffset};
+
+use crate::{objects::tree, plumbing::object::{self, Signature}};
 
 type MessageEncoding = String;
 
@@ -94,4 +96,74 @@ pub struct Commit {
     pub encoding: MessageEncoding,
     // List of extra headers of the commit
     pub extra_headers: Vec<ExtraHeader>,
+}
+
+
+impl Commit {
+    pub fn encode(&self) -> Vec<u8> {
+
+        let mut encoded = Vec::new();
+        let tree_hash = base16ct::lower::encode_string(&self.tree_hash);
+        let tree_hash = format!(
+            "tree {}\n",
+            tree_hash
+        );
+        encoded.extend_from_slice(tree_hash.as_bytes());
+
+        for parent_hash in &self.parent_hashes {
+            let parent_hash_str = base16ct::lower::encode_string(parent_hash);
+            let parent_line = format!("parent {}\n", parent_hash_str);
+            encoded.extend_from_slice(parent_line.as_bytes());
+        }
+
+        encoded.extend_from_slice(
+            self.encode_author().as_slice(),
+        );
+        encoded.extend_from_slice(
+            &self.encode_committer()
+        );
+        encoded.extend_from_slice(b"\n\n");
+        encoded.extend_from_slice(self.message.as_bytes());
+
+        let obj_header = format!(
+            "{} {}\0",
+            object::OBJ_COMMIT_HEADER,
+            encoded.len()
+        );
+
+        let new_encoded = [&obj_header.as_bytes()[..], &encoded[..]].concat();
+        new_encoded
+    }
+
+    pub fn encode_author(&self) -> Vec<u8> {
+        let mut encoded = Vec::new();
+        encoded.extend_from_slice(
+            format!(
+                "author {} <{}> {}\n",
+                self.author.name, self.author.email, String::from_utf8_lossy(&self.encode_when(self.author.when)),
+            )
+            .as_bytes(),
+        );
+        encoded
+    }
+
+    pub fn encode_committer(&self) -> Vec<u8> {
+        let mut encoded = Vec::new();
+        encoded.extend_from_slice(
+            format!(
+                "committer {} <{}> {}\n",
+                self.committer.name, self.committer.email, String::from_utf8_lossy(&self.encode_when(self.committer.when)),
+            )
+            .as_bytes(),
+        );
+        encoded
+    }
+
+    fn encode_when(&self, when: DateTime<FixedOffset>) -> Vec<u8> {
+        let mut encoded = Vec::new();
+        let unix_time = when.timestamp();
+        let timezone = when.format("%z").to_string();
+        encoded.extend_from_slice(format!("{} {}", unix_time, timezone).as_bytes());
+        encoded
+    }
 }
